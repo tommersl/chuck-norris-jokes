1b2a14781beffd672472505df7d37cf2724842d9
tommer levin <tommersl@gmail.com>
2020-04-26 14:30:43 +0300

    Updates

---
+++ precache-manifest.26328e8b03a5aac90935972378a18833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f83f2472b1356229f5e1926fa9817a36",
+    "url": "/chuck-norris-jokes/index.html"
+  },
+  {
+    "revision": "144bb66db9fd3f70570a",
+    "url": "/chuck-norris-jokes/static/css/main.0b3df9a6.chunk.css"
+  },
+  {
+    "revision": "24c1a0f2c7b9e8fb97a9",
+    "url": "/chuck-norris-jokes/static/js/2.74cbb06f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chuck-norris-jokes/static/js/2.74cbb06f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "144bb66db9fd3f70570a",
+    "url": "/chuck-norris-jokes/static/js/main.48a5fb31.chunk.js"
+  },
+  {
+    "revision": "a458cc1af2a827bddf01",
+    "url": "/chuck-norris-jokes/static/js/runtime-main.1c00c556.js"
+  }
+]);
